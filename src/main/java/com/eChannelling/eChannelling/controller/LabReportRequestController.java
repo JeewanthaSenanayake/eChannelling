36077e8a81b6c5629ee0380ee1b7cff7f5a951c4
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-05-27 14:39:29 +0530

    update status  updateUserStatus/{id}/{status}

--- src/main/java/com/eChannelling/eChannelling/controller/LabReportRequestController.java
+++ src/main/java/com/eChannelling/eChannelling/controller/LabReportRequestController.java
@@ -3,16 +3,23 @@ package com.eChannelling.eChannelling.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eChannelling.eChannelling.repository.LabReportRequestRepository;
+
+// import org.apache.hc.core5.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
 import com.eChannelling.eChannelling.entity.LabReportRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/lab_reports")
@@ -42,6 +49,19 @@ public class LabReportRequestController {
             @RequestParam(required = true) String doctorId,
             @RequestParam(required = true) String patientId) {
         return LabReport.findByStatusAndDoctorIdAndPatientId(status, doctorId, patientId);
+    }
+
+    @PutMapping("updateUserStatus/{id}/{status}")
+        public ResponseEntity<String> updateUserStatus(@PathVariable int id, @PathVariable int status) {
+        Optional<LabReportRequest> optionalRequest = LabReport.findById((long) id);
+        if (optionalRequest.isPresent()) {
+            LabReportRequest request = optionalRequest.get();
+            request.setStatus(status);
+            LabReport.save(request);
+            return new ResponseEntity<>("Updated", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+        }
     }
 
 
