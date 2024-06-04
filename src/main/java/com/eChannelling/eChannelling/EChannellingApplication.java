31b9b6b7f416945cf8675ef56ddb708df179f5be
JeewanthaSenanayake <jeewanthamsenanayake@gmail.com>
2024-06-04 23:28:11 +0530

    lab report upload /lab_reports/pdf

--- src/main/java/com/eChannelling/eChannelling/EChannellingApplication.java
+++ src/main/java/com/eChannelling/eChannelling/EChannellingApplication.java
@@ -2,8 +2,7 @@ package com.eChannelling.eChannelling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 public class EChannellingApplication {
